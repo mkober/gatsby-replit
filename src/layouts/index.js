50d1ad70a0776139f6a1d0be379c79404748ba63
Mark Koberlein <mkoberlein@gmail.com>
2021-03-19 07:12:40 +0000

    simplified layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-import './index.css'
 
-const Layout = ({ children, data }) => (
+const Layout = ({ children }) => (
   <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <Header/>
+    {children}
   </div>
 )
 
@@ -33,13 +15,3 @@ Layout.propTypes = {
 }
 
 export default Layout
-
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
